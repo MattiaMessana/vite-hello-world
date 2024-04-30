35cba9f6d3808910ceed40c12b641a7941ed0583
MattiaMessana <mattia.messana@icloud.com>
2024-04-30 18:33:18 +0200

    add: bonus img path

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,9 +1,23 @@
 <script>
-
+    export default {
+        data () {
+            return {
+                title: "La mia prima app con Vite! ",
+                image: "img01.jpg",
+            };
+        },
+        methods: {
+            getImagePath(imageName) {
+                return new URL(`../assets/image/${imageName}`, import.meta.url).href;
+            },
+        },
+    };
 </script>
 
 <template>
-    <h1> La mia prima app con Vite! </h1>
+    <h1> {{ title }} </h1>
+    <img :src="getImagePath(image)" alt="" />
+    
 </template>
 
 <style>
